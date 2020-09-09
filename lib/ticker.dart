b154916a4090804df43c76364f0a3b04880aed1e
djeriko <jerikosalivan@gmail.com>
2020-09-09 10:24:00 +0300

    added Tikcer class and changed pubspec.yaml file

---
+++ lib/ticker.dart
@@ -0,0 +1,7 @@
+// The ticker is our data source for the timer application
+class Ticker {
+  Stream<int> tick({int ticks}) {
+    return Stream.periodic(Duration(seconds: 1), (x) => ticks - x - 1)
+        .take(ticks);
+  }
+}
