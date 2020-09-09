c91e3e9c2333e6f52b02c5d2d267bbe5def24578
djeriko <jerikosalivan@gmail.com>
2020-09-09 10:45:34 +0300

    added timer_event bloc

--- lib/bloc/timer_event.dart
+++ lib/bloc/timer_event.dart
@@ -6,3 +6,30 @@ abstract class TimerEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class TimerStarted extends TimerEvent {
+  final int duration;
+
+  const TimerStarted({@required this.duration});
+
+  @override
+  String toString() => "TimerStarted { duration: $duration }";
+}
+
+class TimerPaused extends TimerEvent {}
+
+class TimerResumed extends TimerEvent {}
+
+class TimerReset extends TimerEvent {}
+
+class TimerTicked extends TimerEvent {
+  final int duration;
+
+  TimerTicked({@required this.duration});
+
+  @override
+  List<Object> get props => [duration];
+
+  @override
+  String toString() => "TimerTicked { duration: $duration }";
+}
